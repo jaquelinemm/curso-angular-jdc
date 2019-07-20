92ae2f1cbc228178f6651f68df4ca483a2049e8c
jaquelinemm <jaquelinemm@gmail.com>
2019-07-20 11:51:23 -0300

    Criação dos primeiros componentes de Hello World e exercícios básicos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HelloWorldComponent} from './components/hello-world/hello-world.component';
+import { HelloWorld2Component} from './components/hello-world2/hello-world2.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path: '', pathMatch: 'full', redirectTo:'primeiro' },
+{path: 'primeiro', component: HelloWorldComponent },
+{path: 'segundo', component: HelloWorld2Component }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HelloWorldComponent } from './components/hello-world/hello-world.component';
+import { HelloWorld2Component } from './components/hello-world2/hello-world2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloWorldComponent,
+    HelloWorld2Component
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
